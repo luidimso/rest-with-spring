73f0e641031f97ed73b52c773b2d1343ea788dd2
Luidi Matheus Silva de Oliveira <luidimatheus.gft@sulamerica.com.br>
2023-10-13 05:30:36 -0300

    first commit

---
+++ src/main/java/com/luidimso/RestWithSpringboot2Application.java
@@ -0,0 +1,13 @@
+package com.luidimso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringboot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/luidimso/RestWithSpringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luidimso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
