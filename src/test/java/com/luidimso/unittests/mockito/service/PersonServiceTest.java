d1acd4f137d2c2494239dec38caa470e310930d3
luidimso <luidi_mso@outlook.com>
2023-10-16 05:55:34 -0300

    Implementing the unit tests fot the others methods: save, update and delete

--- src/test/java/com/luidimso/unittests/mockito/service/PersonServiceTest.java
+++ src/test/java/com/luidimso/unittests/mockito/service/PersonServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.luidimso.data.vo.v1.PersonVO;
 import com.luidimso.interfaces.PersonRepository;
 import com.luidimso.model.Person;
 import com.luidimso.service.PersonService;
@@ -60,24 +61,68 @@ class PersonServiceTest {
 //		fail("Not yet implemented");
 //	}
 //
-//	@Test
-//	void testCreate() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	void testCreate() {
+		// this test makes what the service.save does, it mocks the personVo that it receives as parameter and mocks what repository,save returns, and it should only test the return
+	
+		Person entity = input.mockEntity(0);
+		
+		Person persisted = entity;
+		persisted.setId(1L);
+		
+		PersonVO vo = input.mockVO();
+		vo.setKey(1L);
+		
+		when(repository.save(entity)).thenReturn(persisted);
+		
+		var result = service.create(vo);
+		
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		
+		assertEquals("Addres Test0", result.getAddress());
+		assertEquals("First Name Test0", result.getFirstName());
+		assertEquals("Male", result.getGender());	
+		
+	}
 //
 //	@Test
 //	void testCreateV2() {
 //		fail("Not yet implemented");
 //	}
 //
-//	@Test
-//	void testUpdate() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	void testUpdate() {
+		Person entity = input.mockEntity(0);
+		entity.setId(1L);
+		
+		Person persisted = entity;
+		persisted.setId(1L);
+		
+		PersonVO vo = input.mockVO();
+		vo.setKey(1L);
+		
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		when(repository.save(entity)).thenReturn(persisted);
+		
+		var result = service.update(vo);
+		
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		
+		assertEquals("Addres Test0", result.getAddress());
+		assertEquals("First Name Test0", result.getFirstName());
+		assertEquals("Male", result.getGender());	
+		
+	}
 //
-//	@Test
-//	void testDelete() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	void testDelete() {
+		Person entity = input.mockEntity(0);
+		entity.setId(1L);
+		
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		service.delete(1L);
+	}
 
 }
