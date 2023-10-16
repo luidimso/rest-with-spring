ccd4425369a28a3e7eb40082edb6411b0ec5af12
luidimso <luidi_mso@outlook.com>
2023-10-16 17:14:57 -0300

    Adding TestContainers and Rest-Assured plugis on project

--- src/main/java/com/luidimso/PersonController.java
+++ src/main/java/com/luidimso/PersonController.java
@@ -18,17 +18,41 @@ import com.luidimso.data.vo.v1.PersonVO;
 import com.luidimso.data.vo.v2.PersonVOV2;
 import com.luidimso.service.PersonService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints to managing people database")
 public class PersonController {
 	@Autowired
 	private PersonService service;
 	
+	@Operation(summary = "Finds a person", description = "Finds a person", tags = "People", responses = {
+			@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = PersonVO.class))),
+			@ApiResponse(responseCode = "400", content = @Content),
+			@ApiResponse(responseCode = "401", content = @Content),
+			@ApiResponse(responseCode = "404", content = @Content),
+			@ApiResponse(responseCode = "500", content = @Content)
+	})
 	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE,  MediaType.APPLICATION_XML_VALUE })
 	public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
 		return service.findById(id);
 	}
 	
+	@Operation(summary = "Finds all people", description = "Finds all people", tags = "People", responses = {
+			@ApiResponse(responseCode = "200", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PersonVO.class)))
+			}),
+			@ApiResponse(responseCode = "400", content = @Content),
+			@ApiResponse(responseCode = "401", content = @Content),
+			@ApiResponse(responseCode = "404", content = @Content),
+			@ApiResponse(responseCode = "500", content = @Content)
+	})
 	@GetMapping(produces ={ MediaType.APPLICATION_JSON_VALUE,  MediaType.APPLICATION_XML_VALUE })
 	public List<PersonVO> findAll() throws Exception {
 		return service.findAll();
