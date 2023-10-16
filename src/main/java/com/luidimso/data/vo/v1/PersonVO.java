1fa75e5ffd6c673447127322e146abc2a8a2b057
luidimso <luidi_mso@outlook.com>
2023-10-16 04:33:32 -0300

    Adjusting the VO model to return the ID property

--- src/main/java/com/luidimso/data/vo/v1/PersonVO.java
+++ src/main/java/com/luidimso/data/vo/v1/PersonVO.java
@@ -15,6 +15,7 @@ public class PersonVO extends RepresentationModel<PersonVO> implements Serializa
 
 	private static final long serialVersionUID = 1L;
 	
+	@JsonProperty("id")
 	@Mapping("id")
 	private Long key;
 	@JsonProperty("first_name")
