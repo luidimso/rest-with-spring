1cc7dbc9c9261ff9ec253baf60c0d4f406efb8cb
luidimso <luidi_mso@outlook.com>
2023-10-22 21:08:50 -0300

    Implementing the query params to set the sort direction of the returned data

--- src/main/java/com/luidimso/PersonController.java
+++ src/main/java/com/luidimso/PersonController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -63,8 +65,11 @@ public class PersonController {
 	@GetMapping(produces ={ MediaType.APPLICATION_JSON_VALUE,  MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<Page<PersonVO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "limit", defaultValue = "12") Integer limit) throws Exception {
-		Pageable pageable = PageRequest.of(page, limit);
+			@RequestParam(value = "limit", defaultValue = "12") Integer limit,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) throws Exception {
+		
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "firstName"));
 		
 		return ResponseEntity.ok(service.findAll(pageable));
 	}
